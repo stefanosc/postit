4c6423ee005e653ec9c2c32e7d84ccbc89668021
Stefano Schiavi <stefano@bvprojects.org>
2013-12-02 19:02:31 -0800

    fix models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
   belongs_to :user
   has_many :comments
-  has_many :categories
+  has_many :post_categories
+  has_many :categories, through: :post_categories
 end
