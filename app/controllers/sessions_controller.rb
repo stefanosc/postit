d746f2fe5ae878fd7b2783d071e9ba5ee680602f
Stefano Schiavi <stefano@bvprojects.org>
2013-12-18 15:56:51 +0100

    tidy up authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,9 @@
 class SessionsController < ApplicationController
 
-  before_action :require_guest, except: [:destroy, :create]
+  # before_action :require_guest, except: [:destroy, :create]
 
   def new
-    # redirect_to profile_path if logged_in?
+    redirect_to profile_path if logged_in?
     session[:login_referrer] = request.env["HTTP_REFERER"] if !session[:login_referrer]
   end
 
